89647dfded3a80f36be008ad77bbed1a9fd7d76f
Julio Merino <julio@meroh.net>
2026-05-03 10:56:27 +0200

    Add support for modern AGPL/GPL licenses

--- src/licenses.rs
+++ src/licenses.rs
@@ -16,8 +16,14 @@ use std::fmt;
 #[allow(missing_docs)]
 #[derive(Clone, Copy)]
 pub enum License {
+    AGPL3Only,
+    AGPL3OrLater,
     Apache2,
     BSD3Clause,
+    GPL2Only,
+    GPL2OrLater,
+    GPL3Only,
+    GPL3OrLater,
     MIT,
 }
 
@@ -26,8 +32,14 @@ impl License {
     pub(crate) fn from_cargo() -> Option<Self> {
         // TODO(jmmv): It'd be nice to support "OR" as well and not silently ignore unknown values.
         match env!("CARGO_PKG_LICENSE") {
+            "AGPL-3.0-only" => Some(Self::AGPL3Only),
+            "AGPL-3.0-or-later" => Some(Self::AGPL3OrLater),
             "Apache-2.0" => Some(Self::Apache2),
             "BSD-3-Clause" => Some(Self::BSD3Clause),
+            "GPL-2.0-only" => Some(Self::GPL2Only),
+            "GPL-2.0-or-later" => Some(Self::GPL2OrLater),
+            "GPL-3.0-only" => Some(Self::GPL3Only),
+            "GPL-3.0-or-later" => Some(Self::GPL3OrLater),
             "MIT" => Some(Self::MIT),
             _ => None,
         }
@@ -37,8 +49,26 @@ impl License {
 impl fmt::Display for License {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let text = match self {
+            Self::AGPL3Only => {
+                "GNU Affero General Public License v3.0 only <https://www.gnu.org/licenses/agpl-3.0.html>"
+            }
+            Self::AGPL3OrLater => {
+                "GNU Affero General Public License v3.0 or later <https://www.gnu.org/licenses/agpl-3.0.html>"
+            }
             Self::Apache2 => "Apache Version 2.0 <http://www.apache.org/licenses/LICENSE-2.0>",
             Self::BSD3Clause => "BSD 3-Claus <https://opensource.org/license/bsd-3-clause>",
+            Self::GPL2Only => {
+                "GNU General Public License v2.0 only <https://www.gnu.org/licenses/gpl-2.0.html>"
+            }
+            Self::GPL2OrLater => {
+                "GNU General Public License v2.0 or later <https://www.gnu.org/licenses/gpl-2.0.html>"
+            }
+            Self::GPL3Only => {
+                "GNU General Public License v3.0 only <https://www.gnu.org/licenses/gpl-3.0.html>"
+            }
+            Self::GPL3OrLater => {
+                "GNU General Public License v3.0 or later <https://www.gnu.org/licenses/gpl-3.0.html>"
+            }
             Self::MIT => "MIT <https://opensource.org/license/mit>",
         };
         write!(f, "{}", text)
